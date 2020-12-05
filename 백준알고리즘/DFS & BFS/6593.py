027545f194ce648b6c385d6ad2e6f0b9157e4d98
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2020-12-05 14:46:12 +0900

    20201205 상범 빌딩

---
+++ 백준알고리즘/DFS & BFS/6593.py
@@ -0,0 +1,48 @@
+# 6593 상범 빌딩
+# bfs
+
+from collections import deque
+
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, 1, -1, 0, 0]
+dz = [0, 0, 0, 0, 1, -1]
+
+
+def bfs(x, y, z):
+    q = deque()
+    q.append([x, y, z])
+    d[x][y][z] = 1
+    while q:
+        x, y, z = q.popleft()
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+            if 0 <= nx < l and 0 <= ny < r and 0 <= nz < c:
+                if array[nx][ny][nz] == 'E':
+                    print("Escaped in %d minute(s)." %(d[x][y][z]))
+                    return
+                if array[nx][ny][nz] == '.' and d[nx][ny][nz] == 0:
+                    q.append([nx, ny, nz])
+                    d[nx][ny][nz] = d[x][y][z] + 1
+    return print("Trapped!")
+
+
+while True:
+    # L = z, R = Row(열), C = Column(행)
+    l, r, c = map(int, input().split())
+    if l + r + c == 0:
+        break
+
+    array = [[[] * c for _ in range(r)] for _ in range(l)]
+    d = [[[0]*c for _ in range(r)] for _ in range(l)]
+
+    for i in range(l):
+        array[i] = [list(map(str, input().strip())) for _ in range(r)]
+        input()
+
+    for i in range(l):
+        for j in range(r):
+            for k in range(c):
+                if array[i][j][k] == 'S':
+                    bfs(i, j, k)
