9efb4b04480d850fcb3f68e15eb32665fe01d737
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2020-12-26 15:59:11 +0900

    20201226 1,2,3 더하기

---
+++ 백준알고리즘/DP/9095.py
@@ -0,0 +1,12 @@
+# 9095 1, 2, 3 더하기
+# DP
+
+# An = A(n-1) + A(n+2) + A(n+3)
+t = int(input())
+for _ in range(t):
+    dp = [1, 2, 4]
+    n = int(input())
+
+    for i in range(3, n+1):
+        dp.append(dp[i-1] + dp[i-2] + dp[i-3])
+    print(dp[n-1])
