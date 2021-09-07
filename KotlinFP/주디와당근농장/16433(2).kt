59ff1f0ffc404bf79ca967b61a2952d8dd27e6e1
LeeJangHee <56954702+LeeJangHee@users.noreply.github.com>
2021-09-08 01:44:43 +0900

    코드 정리 및 재귀 함수 수정

---
+++ KotlinFP/주디와당근농장/16433(2).kt
@@ -0,0 +1,66 @@
+package baekjoon
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+class JodiFarm2 {
+    init {
+        input()
+    }
+
+    private fun ArrayList<ArrayList<Char>>.print() = run {
+        this.forEach {
+            it.forEach { ch -> print(ch) }
+            println()
+        }
+    }
+
+    private fun Int.toArrayList() = run {
+        val farmList = ArrayList<ArrayList<Char>>(this)
+        for (i in 0 until this) {
+            val s = ArrayList<Char>()
+            for (j in 0 until this) {
+                s.add('.')
+            }
+            farmList.add(s)
+        }
+        farmList
+    }
+
+    private fun initFarm(n: Int, f: (list: ArrayList<ArrayList<Char>>, r: Int, c: Int) -> ArrayList<ArrayList<Char>>) =
+        { r: Int, c: Int -> f(n.toArrayList(), r, c) }
+
+    private fun growingFarm(list: ArrayList<ArrayList<Char>>, r: Int, c: Int): ArrayList<ArrayList<Char>> {
+        val isCarrot: (Char) -> Boolean = { it == '.' }
+
+        when {
+            r < 0 || r >= list.size || c < 0 || c >= list.size -> return list
+
+            isCarrot(list[r][c]) -> {
+                list[r][c] = 'v'
+                growingFarm(list, r - 1, c - 1)
+                growingFarm(list, r + 1, c + 1)
+                growingFarm(list, r - 1, c + 1)
+                growingFarm(list, r + 1, c - 1)
+            }
+        }
+
+        return list
+    }
+
+    private fun solution(n: Int, r: Int, c: Int) {
+        initFarm(n, ::growingFarm)(r, c).print()
+    }
+
+    private fun input() = with(Scanner(System.`in`)) {
+        val n = nextInt()
+        val r = nextInt() - 1
+        val c = nextInt() - 1
+
+        solution(n, r, c)
+    }
+}
+
+fun main() {
+    JodiFarm2()
+}
